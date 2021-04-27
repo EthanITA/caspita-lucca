aa57367abfd0ad7f964c8b2f32178ff767ba6ff4
Marco <viaimperiale@gmail.com>
2021-04-28 00:02:51 +0200

    auto deploy

--- github_deploy.sh
+++ github_deploy.sh
@@ -1,1 +1,1 @@
-npm run build && git push CaspitaSrl `git subtree split --prefix app/dist master`:gh-pages --force
+npm run build && git add app/dist && git commit -a  && git push CaspitaSrl `git subtree split --prefix app/dist master`:gh-pages --force
