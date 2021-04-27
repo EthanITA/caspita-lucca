539937b93e884f882ca314c766b17c89d95ece4a
Marco <viaimperiale@gmail.com>
2021-04-27 19:24:34 +0200

    brand_logo.png updated and using <nav>

--- app/src/components/Homepage.vue
+++ app/src/components/Homepage.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
+    <nav class="navbar docs-navbar is-spaced has-shadow is-primary is-transparent">
+      <div class="container">
+        <div class="navbar-brand is-vcentered">
+          <img
+            src="../assets/brand_logo.png"
+            alt="Caspita SRL brand" style="height: 3.25rem; width:auto"
+          />
+        </div>
+      </div>
+    </nav>
     <section class="section hero is-fullheight is-link">
       <div class="hero-head ">
         <b-navbar>
-          <template #brand>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-              <b-image
-                :src="require(`@/assets/logo.png`)"
-                alt="Caspita SRL brand"
-              />
-            </b-navbar-item>
-          </template>
         </b-navbar>
       </div>
       <p>{{ this.$store.state.ping }}</p>
