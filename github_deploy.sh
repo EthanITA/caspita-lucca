ba85f03f2ce06290c7db83c91ae73bc5d1088b9d
Marco <viaimperiale@gmail.com>
2021-04-28 16:36:33 +0200

    dist folder ignored and updated github deploying shell script accordingly

--- github_deploy.sh
+++ github_deploy.sh
@@ -1,1 +1,3 @@
-npm run build && git add app/dist && git commit -a  && git push CaspitaSrl `git subtree split --prefix app/dist master`:gh-pages --force
+npm run build
+git add app/dist -f
+git commit -m "automatic deploy"  && git push CaspitaSrl `git subtree split --prefix app/dist master`:gh-pages --force
