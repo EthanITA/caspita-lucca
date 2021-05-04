df973c31f7bae16986356c2f8cc27a263aa9cc82
Marco <viaimperiale@gmail.com>
2021-05-04 13:10:34 +0200

    google cloud implementation on server side

--- server/init_flask.py
+++ server/init_flask.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from flask_cors import CORS
 
-
-def get_flask_app():
-    app = Flask(__name__)
-    CORS(app, resources={r'/*': {'origins': '*'}})
-    return app
+app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
