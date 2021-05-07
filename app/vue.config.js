e3c483c6ec4a4570a4a8b24dc8048032ce70c942
Marco <viaimperiale@gmail.com>
2021-05-07 18:26:40 +0200

    customized PWA settings

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/lucca'
-    : '/'
+    : '/',
+  pwa: {
+    name: 'Caspita',
+    themeColor: '#D777DD',
+    msTileColor: '#FFFFFF'
+  }
 }
