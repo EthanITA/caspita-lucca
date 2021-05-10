c7a35e775a721571c602f1fb8385be44defebbdd
Marco <viaimperiale@gmail.com>
2021-05-10 22:08:05 +0200

    reverting using toolbar component in order to fill 100vh

--- app/src/components/home/homepage.vue
+++ app/src/components/home/homepage.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <loader/>
-    <toolbar v-show="this.$store.state.homepage_loader_loaded" class="ani-slide-in-left ani-800"/>
     <div v-show="this.$store.state.homepage_loader_loaded" class="ani-slide-in-left ani-800">
-      <md-app style="height:auto;" mode="fixed">
-        <md-divider/>
+      <md-app style="height: 100vh;">
+        <md-app-toolbar>
+          <div class="md-toolbar-row">
+            <div class="md-toolbar-section-start">
+              <brand_logo/>
+            </div>
+            <div class="md-toolbar-section-end">
+              <menu_option/>
+            </div>
+          </div>
+        </md-app-toolbar>
         <md-app-content class="hero" style="padding: 0">
           <homepage_body class="hero-body"/>
         </md-app-content>
@@ -24,11 +32,12 @@
 import Below_body from "@/components/home/below_body";
 import Homepage_body from "./body";
 import Loader from "./loader";
-import Toolbar from "@/components/header/toolbar";
+import Brand_logo from "@/components/header/caspita_logo";
+import Menu_option from "@/components/header/menu";
 
 export default {
   name: 'homepage',
-  components: {Toolbar, Loader, Homepage_body, Below_body},
+  components: {Menu_option, Brand_logo, Loader, Homepage_body, Below_body},
 };
 </script>
 <style scoped>
